24e9698686464d31ccf1ce8e3d92c37ec22672c0
sayamapp <sayamapp@gmail.com>
2021-03-13 09:58:19 +0900

    aaaa

--- abc194/src/bin/a.rs
+++ abc194/src/bin/a.rs
@@ -1,4 +1,15 @@
 use proconio::input;
 use proconio::marker::*;
 fn main() {
+    input! {
+        a: usize,
+        b: usize,
+    }
+    let c = a + b;
+    let res =
+        if c >= 15 && b >= 8 {1}
+        else if c >= 10 && b >= 3 {2}
+        else if c >= 3 {3}
+        else {4};
+    println!("{}", res);
 }
